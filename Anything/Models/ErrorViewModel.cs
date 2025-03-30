8ffda4dfe935db1ed291b5000cdd85e1022047aa
Nidhi Gaonkar <95069047+grnidhi@users.noreply.github.com>
2025-03-30 11:37:57 +0530

    Delete Anything directory

--- Anything/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Anything.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
